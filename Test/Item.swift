fbff2cf90d8868a7da993a6998f36b1e46b22f1f
Jilian King <jilianmk70@gmail.com>
2026-01-13 11:08:27 -0500

    Initial Commit

---
+++ Test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Test
+//
+//  Created by jilian king on 1/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
